c9103d0236580971a6cfc94fe6a754f5dac5cca4
Jackson Silva <163166933+ogJackson1@users.noreply.github.com>
2024-04-23 10:05:04 -0300

    Add files via upload

---
+++ CodigoaulaDs/src/PessoaFisica.java
@@ -0,0 +1,10 @@
+public class PessoaFisica extends Pessoa{
+    private String rg;
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+    private String getRg(){
+        return (rg);
+    }
+}
