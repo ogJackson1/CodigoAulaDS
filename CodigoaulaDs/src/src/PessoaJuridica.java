0632187b704eb7f1b02a0423e6c0fa2d6314a496
Jackson Silva <163166933+ogJackson1@users.noreply.github.com>
2024-04-30 09:15:36 -0300

    Delete CodigoaulaDs/src/src directory

--- CodigoaulaDs/src/src/PessoaJuridica.java
+++
@@ -1,9 +0,0 @@
-public class PessoaJuridica extends Pessoa{
-    private String cnpj;
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
-    }
-    private String getCnpj(){
-        return (cnpj);
-    }
-}
--- CodigoaulaDs/src/src/UsaFuncionario.java
+++
@@ -1,9 +0,0 @@
-public class UsaFuncionario {
-    public static void main(String[] args){
-        Funcionario funcionario =  new Funcionario();
-        funcionario.setNome("Lucas");
-        funcionario.setRg("25.654.678-x");
-        funcionario.setCartao("RH845");
-
-    }
-}
