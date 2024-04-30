39250ac8501f2f9795759d15058696548115e134
Jackson Silva <163166933+ogJackson1@users.noreply.github.com>
2024-04-30 09:23:18 -0300

    Delete src/Funcionario.java

--- src/Funcionario.java
+++
@@ -1,9 +0,0 @@
-public class Funcionario extends PessoaFisica{
-    private String cartao;
-    public void setCartao(String cartao) {
-        this.cartao = cartao;
-    }
-    public String getCartao() {
-        return cartao;
-    }
-}
