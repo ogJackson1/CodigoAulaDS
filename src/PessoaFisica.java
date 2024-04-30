65b0b145dded0a94793ed8bc16212ff2d5cd8823
Jackson Silva <163166933+ogJackson1@users.noreply.github.com>
2024-04-30 09:23:58 -0300

    Delete src/PessoaFisica.java

--- src/PessoaFisica.java
+++
@@ -1,9 +0,0 @@
-public class PessoaFisica extends Pessoa{
-    private String rg;
-    public void setRg(String rg) {
-        this.rg = rg;
-    }
-    private String getRg(){
-        return (rg);
-    }
-}
