8e3c8aaa72696c83cce912cf952844e6901dde84
Jackson Silva <163166933+ogJackson1@users.noreply.github.com>
2024-06-04 09:31:45 -0300

    Add files via upload

---
+++ CodigoIf/If.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import javax.swing.JOptionPane;
+public class If {
+    public static void main(String[] args){
+        Object[] op = {"masculino", "feminino"};
+        String resp = (String) JOptionPane.showInputDialog(null,
+                "Selecione o sexo:\n", "Pesquisa", JOptionPane.PLAIN_MESSAGE,
+                null, op, "Masculino");
+
+        if (resp==null) {
+            JOptionPane.showMessageDialog(null, "você pressionou Cancel!");
+        }
+        if (resp.equals("Masculino")) {
+            JOptionPane.showMessageDialog(null, "Você é homem!");
+        }
+        if (resp.equals("Feminino")) {
+            JOptionPane.showMessageDialog(null, "Você é mulher!");
+        }
+        System.exit(  0);
+    }
+}
